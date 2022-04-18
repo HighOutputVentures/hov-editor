5e1ec270ec779afd00bab485df8a67bc1de74152
pailadin <21221101+pailadin@users.noreply.github.com>
2022-04-18 15:22:42 +0800

    feat: marks set on ingest

--- core/src/library/TextCruncher.ts
+++ core/src/library/TextCruncher.ts
@@ -1,26 +1,56 @@
 enum MarkType {
-  TripleAsterisk,
-  DoubleAsterisk,
-  Asterisk,
+  DoubleAsterisk = "**",
+  Asterisk = "*",
+  DoubleUnderscore = "__",
+  Underscore = "_",
 }
 
-type Mark = {
-  type: MarkType;
-  index: number;
-};
+const MarkMap: { [key: string]: string[] } = {
+  [MarkType.DoubleAsterisk]: ["<b>", "</b>"],
+  [MarkType.Asterisk] : ["<i>", "</i>"],
+  [MarkType.DoubleUnderscore]: ["<b>", "</b>"],
+  [MarkType.Underscore] : ["<i>", "</i>"],
+}
 
 export class TextCruncher {
-  private marks: Mark[] = [];
+  private marks: MarkType[] = [];
 
   private _text: string = '';
 
-  public reset() {}
+  public reset() {
+    this._text = '';
+    this.marks = [];
+  }
+
+  private getPrevMarkAndIndex = (): [MarkType, number] =>
+    [this.marks.splice(-1)[0], this.marks.length - 1]
 
   public ingest(c: string) {
+    let marksUpdated = false;
+
+    const [prevMark, prevMarkIndex] = this.getPrevMarkAndIndex();
+
+    if (prevMark !== undefined) {
+      const prevMarkInput = MarkMap[prevMark];
+
+      if ((prevMarkInput + c) in MarkMap) {
+        this.marks[prevMarkIndex] = prevMarkInput + c as MarkType;
+        marksUpdated = true;
+      }
+    }
+
+    if (!marksUpdated) {
+      if (c in MarkMap) {
+        this.marks.push(c as MarkType);
+      }
+    }
+
     this._text += c;
   }
 
   public bulkIngest(s: string) {
+    this.marks = [];
+
     for (const c of s) {
       this.ingest(c);
     }

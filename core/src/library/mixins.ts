98f0fabedc998ead8a1f91a9e085549c8c9c7630
pailadin <21221101+pailadin@users.noreply.github.com>
2022-04-20 15:21:01 +0800

    feat: mixins util

---
+++ core/src/library/mixins.ts
@@ -0,0 +1,13 @@
+// https://www.digitalocean.com/community/tutorials/typescript-mixins:
+export default function applyMixins(derivedCtor: any, constructors: any[]) {
+  constructors.forEach((baseCtor) => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+      Object.defineProperty(
+        derivedCtor.prototype,
+        name,
+        Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
+          Object.create(null)
+      );
+    });
+  });
+}
